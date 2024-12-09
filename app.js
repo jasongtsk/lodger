5a9c923c64b81f0b541b24c520dc2fc847c7ee2a
Jasongtsk <150237672+Jasongtsk@users.noreply.github.com>
2024-12-09 12:58:12 +0000

    refactor(app.js): Allow CORS

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use('/', index);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
